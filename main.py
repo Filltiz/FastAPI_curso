836d1ca6a941853881595b71e560f2084cf822b7
Filltiz <116226382+Filltiz@users.noreply.github.com>
2026-04-13 18:07:55 -0300

    cadastro

--- main.py
+++ main.py
@@ -1,7 +1,17 @@
 # No terminal:  uvicorn main:app --reload
+import os
+
 from fastapi import FastAPI
+from passlib.context import CryptContext
+from dotenv import load_dotenv
+
+load_dotenv()
+
+SECRET_KEY = os.getenv("SECRET_KEY")
 
 app = FastAPI()
+
+bcrypt_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
 
 from auth import auth_router
 from routes import order_router
